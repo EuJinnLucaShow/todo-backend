4fc59d80247cd2c50b980097c72c636c17a84ccd
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2024-01-02 23:16:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,62 @@
 import express, { json } from 'express';
 import { connect, Schema, model } from 'mongoose';
-require('dotenv').config(); 
-const DB_HOST = process.env.DB_HOST
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const DB_HOST = process.env.DB_HOST;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Підключення до бази даних MongoDB
-connect(`${DB_HOST}`, {
-  
-});
+// Connect to the MongoDB database
+connect(DB_HOST)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
 
-// Схема та модель для Todo
+// Schema and model for Todo
 const todoSchema = new Schema({
-    title: String,
-    description: String,
-    completed: Boolean,
+  title: String,
+  description: String,
+  completed: Boolean,
 });
 
 const Todo = model('Todo', todoSchema);
 
 app.use(json());
 
-// Роути
-
-// Отримати список всіх todo
+// Routes...
+// Get all todos
 app.get('/api/todos', async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        res.json(todos);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// Створити новий todo
+// Create a new todo
 app.post('/api/todos', async (req, res) => {
-    const { title, description, completed } = req.body;
-    const todo = new Todo({
-        title,
-        description,
-        completed,
-    });
+  const { title, description, completed } = req.body;
+  const todo = new Todo({
+    title,
+    description,
+    completed,
+  });
 
-    try {
-        const newTodo = await todo.save();
-        res.status(201).json(newTodo);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-// Запуск сервера
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Сервер запущено на порті ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
